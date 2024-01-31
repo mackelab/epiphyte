4ba224229ce803b8431d5ae7a5a350ba48b06af2
Alana <alana.darcher@gmail.com>
2024-01-31 13:08:40 +0100

    tutorials: database creation

--- src/epiphyte/database/access_info.py
+++ src/epiphyte/database/access_info.py
@@ -1,9 +1,10 @@
+import os
 import datajoint as dj
 
 dj.config["enable_python_native_blobs"] = True
-dj.config['database.host'] = '127.0.0.1'
-dj.config['database.user'] = "root"
-dj.config['database.password'] = 'simple'
+dj.config['database.host'] = '127.0.0.1'            # localhost address, addresses the computer the code is run from 
+dj.config['database.user'] = "root"                 # default username
+dj.config['database.password'] = 'simple'           # default password, change before actually using a database
 
 epi_schema = dj.schema('epiphyte_mock')
 
