73509421b3144d9860e9afa19536b7007d74c65a
Alana <alana.darcher@gmail.com>
2024-02-04 15:14:49 +0100

    setup.py edit

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="epiphyte", 
-    version="0.1.1",
-    author="Alana Darcher, Tamara Mueller",
-    author_email="alana.darcher@gmail.com",
-    description="a Python toolkit for high-dimensional neural data recorded during naturalistic, continuous stimuli",
-    long_description="a Python toolkit for high-dimensional neural data recorded during naturalistic, continuous stimuli",
-    long_description_content_type="text/markdown",
-    url="https://github.com/mackelab/epiphyte",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+setup(
+    name='epiphyte',
+    version='0.1.0', 
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'datajoint>=0.14.1 ',
+        'numpy>=1.26.3',
+        'pandas>=2.2.0'
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7, <3.9',
+    author='Alana Darcher, Tamara Mueller, Rachel Rapp, Franziska Gerken',
+    author_email='alana.darcher@gmail.com',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mackelab/epiphyte',
+    license='MIT', 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
